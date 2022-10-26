d6574f04174edf2a668d0ad14143998bdf931fd7
sdancer <ktion23@gmail.com>
2022-10-26 11:06:20 +0800

    3 files modified

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 // Import stylesheets
 import './style.css';
 
+import * as gleam_wasm from './gl';
+gleam_wasm.init(true);
+gleam_wasm.compile('something');
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
